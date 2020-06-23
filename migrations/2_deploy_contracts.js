e5f4e901824abfeee7ceeb5d1cc30db4ae7f73af
Siddharth Barpanda <siddharthb98@gmail.com>
2020-06-23 20:42:43 +0530

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,5 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
 var Trustful = artifacts.require("./Trustful.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Trustful);
-  // deployer.deploy(ConvertLib);
-  // deployer.link(ConvertLib, MetaCoin);
-  // deployer.deploy(MetaCoin);
 };
